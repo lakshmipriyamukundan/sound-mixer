49a92ec1e6abe9b3ad3c8088a51720843a65d4c4
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2018-04-12 14:32:14 +0530

    cluster module added for forking process

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 const cmd = require('node-cmd');
-const Promise = require('bluebird');
+const cluster = require('cluster');
+//const Promise = require('bluebird');
 const winston = require('winston');
 const logger = require('./logger');
+const { fork } = require('child_process');
+var numCPUs = require('os').cpus().length;
 
 //Promisifying the cmd command
-const getAsync = Promise.promisify(cmd.get);
+// const getAsync = Promise.promisify(cmd.get);
 
-let mixer = async () => {
-	// Defined path for mixing video and audio files.
-	const vPath = './move-it.mp4';
-	const aPath = './song1.mp3';
-	const mPath = 'mixed.mp4';
+// let mixer = async () => {
+// 	// Defined path for mixing video and audio files.
+// 	const vPath = './move-it.mp4';
+// 	const aPath = './song1.mp3';
+// 	const mPath = 'mixed.mp4';
 
-	try {
-		logger.debug('Debug statement');
-		logger.info('Info statement');
+// 	try {
+// 		logger.debug('Debug statement');
+// 		logger.info('Info statement');
 
-		await getAsync(
-			`ffmpeg -i ${vPath} -i ${aPath} -map 0:v -map 1:a -c copy ${mPath}`
-		);
-	} catch (err) {
-		logger.log(err);
+// 		await getAsync(
+// 			`ffmpeg -i ${vPath} -i ${aPath} -map 0:v -map 1:a -c copy ${mPath}`
+// 		);
+// 	} catch (err) {
+// 		logger.log(err);
+// 	}
+// };
+
+let mixer = () => {
+	for (i = 0; i <= numCPUs; i++) {
+		const childMixer = fork('mix.js');
+		childMixer.send('start');
+		childMixer.on('message', sum => {
+			logger.info('Completed');
+		});
 	}
 };
 
---
+++ mix.js
@@ -0,0 +1,13 @@
+const cmd = require('node-cmd');
+const logger = require('./logger');
+
+const vPath = './move-it.mp4';
+const aPath = './song1.mp3';
+const mPath = 'mixed.mp4';
+
+process.on('message', msg => {
+	logger.info('ethi', process.pid);
+	cmd.get(
+		`ffmpeg -i ${vPath} -i ${aPath} -map 0:v -map 1:a -c copy ${mPath}`
+	);
+});
